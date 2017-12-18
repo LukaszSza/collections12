5dbeaf6cbd0c5d9a71252cdbd318b2218b4d16e9
LukaszSz <szaconlukasz@wp.pl>
2017-12-18 20:26:40 +0100

    Change file

--- src/main/java/pl/sda/lukasz/SetOperations.java
+++ src/main/java/pl/sda/lukasz/SetOperations.java
@@ -1,17 +1,21 @@
 package pl.sda.lukasz;
 
+import com.sun.glass.ui.Size;
+
 import java.util.*;
 
 
 public class SetOperations {
+    //    private static final SizeComparator SIZECOMPARATOR = new SizeComparator();
     public static void main(String[] args) {
+
         System.out.println("------HasSet------");
         Set<String> set = new HashSet<String>();
         fillSet(set);
         showSet(set);
 
         System.out.println("------TreeSet-----");
-        Set<String> set2 = new TreeSet<String>();
+        Set<String> set2 = new TreeSet<String>(new SizeComparator());
         fillSet(set2);
         showSet(set2);
 
@@ -21,14 +25,12 @@ public class SetOperations {
         showSet(set3);
 
 
-
     }
 
     private static void showSet(Set<String> set) {
         for (String x : set) {
             System.out.println(x);
         }
-
     }
 
     private static void fillSet(Set<String> set) {
